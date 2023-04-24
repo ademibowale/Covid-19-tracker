0093a303b42054d5cb1b6e86130c6f9c7bba8161
Your Name <you@example.com>
2023-04-24 10:45:23 +0100

    remove bug from app.jsg

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';
-// import Navbar from './components/Navbar';
-// import Homepage from './components/Homepage';
-// import Details from './components/Details';
+
 
 function App() {
-  // const [id, setId] = useState('');
-  // const handleDetail = (e) => {
-  // setId(e.target.id);
-  // };
+  
   return (
     <>
-      {/* <Navbar /> */}
+      
       <Routes>
         <Route path="/" element={<ErrorPage />} />
-        {/* <Route path="/" element={<Homepage handleDetail={handleDetail} />} />
-        <Route path="/details" element={<Details id={id} />} /> */}
+      
       </Routes>
     </>
   );
