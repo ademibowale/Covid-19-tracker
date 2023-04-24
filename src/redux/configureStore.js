4c217b0a112bdb582058822b59742e51ba09bfde
Your Name <you@example.com>
2023-04-24 10:43:03 +0100

    modify homepage.csse

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,10 @@ import thunk from 'redux-thunk';
 import countriesReducer from './Countries/countries';
 import regionsReducer from './Regions/regions';
 
+const reducer = combineReducers({
+  countriesReducer,
+  regionsReducer,
+});
 
 const store = createStore(
   reducer,
